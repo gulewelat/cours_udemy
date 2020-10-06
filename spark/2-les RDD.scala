821caf755c4620473ce518bed54598d4ed36c4dc
Jülide Yilmaz <yilmaz.julide@gmail.com>
2020-10-06 12:50:17 +0200

    continuer le cours

--- spark/2-les RDD.scala
+++ spark/2-les RDD.scala
@@ -1,6 +1,8 @@
 // Les RDDs
 
 // 6.2 Apprendre les bases de la programmation des RDDs
+// 6.3 map, flatMap, filter, distinct, groupBy et sample
+// 6.4 Union, intersection, substract, cartesian, reduce et fold
 
 
 
@@ -61,3 +63,39 @@ distFile2.getNumPartitions
 val rdd = sc.textFile('chemin/ttt.txt') //RDD créé
 
 rdd.count() // count() est une action, chargement des données
+
+
+
+// 6.3 map, flatMap, filter, distinct, groupBy et sample
+
+val rdd = sc.textFile("/Users/julide/Desktop/projets python/spark/ressources/demo.txt")
+
+rdd.collect //affiche le contenu du RDD et chaque ligne est un élément dan sun tableau
+
+val capital = rdd.map(x => x.toUpperCase())
+
+val rdd = sc.parallelize(List(List(1,2), List(3,4)))
+
+rdd.map(x => x).collect
+
+rdd.flatMap(x => x).collect // ne print pas en liste mais met tout dans un seul tableau
+
+val rdd = sc.parallelize(List("Pierre", "Alpha", "Medhi", "Jean-Pierre", "Thierry"))
+val rddPrenomComposes = rdd.filter(x => x.contains("-"))
+rddPrenomComposes.collect
+
+val rdd = sc.parallelize(List("Pierre", "Alpha", "Medhi", "Jean-Pierre", "Thierry", "Pierre"))
+val rddDistinct = rdd.distinct()
+rddDistinct.collect
+val rddGroupBy = rdd.groupBy(x => x.charAt(0))
+
+val rdd = sc.parallelize(1 to 100000)
+rdd.count
+val sampledData = rdd.sample(false, 0.5) // false = que des valeurs uniques, pas de doublons
+// 0,5: je dois récupérer 50% + ou - de mon échantillon
+sampledData.count
+
+// 6.4 Union, intersection, substract, cartesian, reduce et fold
+
+
+
